500ed40205d5ceca049dfef9309be1bb0aecb0d4
Petru Stoica <petrustoica2006@gmail.com>
2025-02-10 16:46:43 +0200

    Change SimpleOpMode to run on LinearOpMode

--- TeamCode/src/main/java/org/simplicityftc/util/SimpleOpMode.java
+++ TeamCode/src/main/java/org/simplicityftc/util/SimpleOpMode.java
@@ -1,47 +1,50 @@
 package org.simplicityftc.util;
 
 import com.qualcomm.hardware.lynx.LynxModule;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.configuration.LynxConstants;
 
 import org.simplicityftc.commandbase.CommandScheduler;
 import org.simplicityftc.electronics.Hub;
-import org.simplicityftc.logger.Logger;
 
-public abstract class SimpleOpMode extends OpMode {
-    public static CommandScheduler commandScheduler;
-    public static Logger logger = Logger.getInstance();
+public abstract class SimpleOpMode extends LinearOpMode {
+    public static CommandScheduler commandScheduler = CommandScheduler.getInstance();
 
-    public void initialize() { }
+    public void onInit() { }
 
     public void initialize_loop() { }
+
+    public void onStart() { }
 
     public void run() { }
 
     @Override
-    public final void init() {
-        for(LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+    public void runOpMode() throws InterruptedException {
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
             if(module.isParent() && LynxConstants.isEmbeddedSerialNumber(module.getSerialNumber())) {
                 Hub.CONTROL_HUB.setHub(module);
             } else {
                 Hub.EXPANSION_HUB.setHub(module);
             }
         }
-        commandScheduler = CommandScheduler.getInstance();
-        initialize();
-    }
 
-    @Override
-    public final void init_loop() {
-        initialize_loop();
-    }
+        commandScheduler.reset();
 
-    @Override
-    public final void loop() {
-        run();
-        CommandScheduler.getInstance().run();
-        Hub.CONTROL_HUB.getLynxModule().clearBulkCache();
-        Hub.EXPANSION_HUB.getLynxModule().clearBulkCache();
-        //TODO: Log update rate
+        onInit();
+
+        while (!isStarted() && !isStopRequested()) {
+            initialize_loop();
+        }
+
+        waitForStart();
+        onStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            run();
+            CommandScheduler.getInstance().run();
+            Hub.CONTROL_HUB.getLynxModule().clearBulkCache();
+            Hub.EXPANSION_HUB.getLynxModule().clearBulkCache();
+            //TODO: Log update rate
+        }
     }
 }
